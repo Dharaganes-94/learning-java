508e916080f92dad43e15deb5ba44bcb3bb54df8
dharani <dharani.gsd@gmail.com>
2023-09-07 15:51:29 +0530

    first commit

---
+++ src/Basics/Module.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class Module {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		int a = 6;
+		int b = 3;
+		int c = a%b;
+		System.out.println("The modulo value is"+c);
+	}
+
+}
---
+++ src/Basics/Multiplication.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 5;
+		int b = 10;
+		int c = a*b;
+		System.out.println("The multiplication value is"+ c);
+
+	}
+
+}
